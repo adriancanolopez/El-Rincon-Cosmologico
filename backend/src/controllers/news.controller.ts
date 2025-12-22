86e226ac405f167bd58848897606f4c041afe705
adriancanolopez <canocanoadrian796@gmail.com>
2025-12-22 01:39:42 +0100

    feat: Implementación de la petición en el backend para obtener todas las noticias.

--- backend/src/controllers/news.controller.ts
+++ backend/src/controllers/news.controller.ts
@@ -1,12 +1,22 @@
 import type { Request, Response } from "express";
 import News from "../models/News.model.ts";
 
+export const getNews = async (req: Request, res: Response) => {
+    try {
+        const news = await News.find();
+        return res.status(200).json(news);
+    } catch (error: unknown) {
+        const message = error instanceof Error ? error.message : String(error); // En caso de que sea un error lanzado por mongoose, error será una instancia de Error.
+        return res.status(500).json({ message: "Error al obtener las noticias. Error: " + message });
+    }
+}
+
 export const createNews = async (req: Request, res: Response) => {
     try {
         const news = await News.create(req.body);
         return res.status(201).json(news);
     } catch (error: unknown) {
-        const message = error instanceof Error ? error.message : String(error); // En caso de que sea un error lanzado por mongoose, error será una instancia de Error.
+        const message = error instanceof Error ? error.message : String(error);
         return res.status(500).json({ message: "Error al crear la noticia. Error: " + message });
     }
 }
--- backend/src/routes/news.routes.ts
+++ backend/src/routes/news.routes.ts
@@ -1,10 +1,11 @@
 import express, { type NextFunction, type Request, type Response } from "express";
-import { createNews, updateNews, deleteNews } from "../controllers/news.controller.ts";
+import { getNews, createNews, updateNews, deleteNews } from "../controllers/news.controller.ts";
 import { upload } from "../config/multer.ts";
 import { MulterError } from "multer";
 
 const router = express.Router();
 
+router.get('/get-news', getNews);
 router.post('/create', createNews);
 router.post('/create-with-image', (req: Request, res: Response, next: NextFunction) => {
     upload.single('image')(req, res, function(error: unknown) {
