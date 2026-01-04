1739ca8940cb24e0bb2a9990f7e28b140e5aa874
adriancanolopez <canocanoadrian796@gmail.com>
2026-01-04 20:06:16 +0100

    feat(client): Creación de la página para ver una noticia y poder editarla, en la parte de administrador.

--- client/src/api/NewsRequests.ts
+++ client/src/api/NewsRequests.ts
@@ -1,4 +1,4 @@
-import type { News, GetNewsResponse, CreateResponse } from "../types/news.types";
+import type { News, GetNewsResponse, CreateResponse, UpdateResponse } from "../types/news.types";
 
 export async function getNews(): Promise<GetNewsResponse> {
     const request = await fetch('http://localhost:3000/news/get-news');
@@ -66,3 +66,54 @@ export async function createNewsWithImage(news: News): Promise<CreateResponse> {
     return response;
 }
 
+export async function updateNews(news: News): Promise<UpdateResponse> {
+
+    const request = await fetch(`http://localhost:3000/news/update/${news._id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(news),
+    });
+
+    const data = await request.json();
+    
+    let response: UpdateResponse;
+
+    if (request.ok) {
+        response = {ok: true, data: data};
+    }
+    else {
+        response = {ok: false, message: data.message};
+    }
+    return response;
+}
+
+export async function updateNewsWithImage(news: News): Promise<UpdateResponse> {
+    
+    const formData = new FormData();
+    const image = news.imageUrl as File;
+    formData.append('title', news.title);
+    formData.append('description', news.description);
+    formData.append('image', image);
+    formData.append('published', String(news.published));
+
+    console.log(formData);
+
+    const request = await fetch(`http://localhost:3000/news/update-with-image/${news._id}`, {
+        method: 'PATCH',
+        body: formData,
+    });
+
+    const data = await request.json();
+    
+    let response: UpdateResponse;
+
+    if (request.ok) {
+        response = {ok: true, data: data};
+    }
+    else {
+        response = {ok: false, message: data.message};
+    }
+    return response;
+}
--- client/src/types/news.types.ts
+++ client/src/types/news.types.ts
@@ -1,12 +1,15 @@
 export interface News {
+    _id?: string,
     title: string,
     description: string,
-    imageUrl: File | null | string,
+    imageUrl?: File | null | string,
     published: boolean,
     createdAt?: string,
     updatedAt?: string
 }
 
 export type CreateResponse = | { ok: true, data: News } | { ok: false, message: string };
+
+export type UpdateResponse = CreateResponse;
 
 export type GetNewsResponse = | { ok: true, data: News[] } | { ok: false, message: string }
